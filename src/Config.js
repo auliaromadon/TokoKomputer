008571b69060bd1d4455bcca0d05447de62299a4
auliaromadon <aulia_romadon_27rpl@smktelkom-mlg.sch.id>
2022-01-05 16:53:15 +0700

    frontend

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+const base_url = "http://localhost:7000/store/api/v1"
+const product_image_url = "http://localhost:7000/product_image/"
+const customer_image_url = "http://localhost:7000/image/"
+export {
+    base_url,
+    product_image_url,
+    customer_image_url
+}
